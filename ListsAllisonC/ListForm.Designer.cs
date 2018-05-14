d5e7a1f291ed1480d3e982ea8e1e6666de99e79e
Allison <allison.cook@ocsbstudent.ca>
2018-05-14 14:03:42 -0400

    half

--- ListsAllisonC/ListForm.Designer.cs
+++ ListsAllisonC/ListForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace ListsAllisonC
 {
-    partial class Form1
+    partial class frmList
     {
         /// <summary>
         /// Required designer variable.
@@ -28,12 +28,97 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txtMark = new System.Windows.Forms.TextBox();
+            this.btnEnter = new System.Windows.Forms.Button();
+            this.lblEnter = new System.Windows.Forms.Label();
+            this.lstMarks = new System.Windows.Forms.ListBox();
+            this.btnAverage = new System.Windows.Forms.Button();
+            this.lblAverage = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtMark
+            // 
+            this.txtMark.Location = new System.Drawing.Point(45, 40);
+            this.txtMark.Name = "txtMark";
+            this.txtMark.Size = new System.Drawing.Size(100, 20);
+            this.txtMark.TabIndex = 0;
+            // 
+            // btnEnter
+            // 
+            this.btnEnter.Font = new System.Drawing.Font("EuroRoman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.btnEnter.Location = new System.Drawing.Point(188, 33);
+            this.btnEnter.Name = "btnEnter";
+            this.btnEnter.Size = new System.Drawing.Size(75, 30);
+            this.btnEnter.TabIndex = 1;
+            this.btnEnter.Text = "Enter";
+            this.btnEnter.UseVisualStyleBackColor = true;
+            this.btnEnter.Click += new System.EventHandler(this.btnEnter_Click);
+            // 
+            // lblEnter
+            // 
+            this.lblEnter.AutoSize = true;
+            this.lblEnter.Font = new System.Drawing.Font("EuroRoman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.lblEnter.Location = new System.Drawing.Point(28, 9);
+            this.lblEnter.Name = "lblEnter";
+            this.lblEnter.Size = new System.Drawing.Size(217, 17);
+            this.lblEnter.TabIndex = 2;
+            this.lblEnter.Text = "Enter a mark between 0 and 100";
+            // 
+            // lstMarks
+            // 
+            this.lstMarks.FormattingEnabled = true;
+            this.lstMarks.Location = new System.Drawing.Point(83, 69);
+            this.lstMarks.Name = "lstMarks";
+            this.lstMarks.Size = new System.Drawing.Size(135, 108);
+            this.lstMarks.TabIndex = 3;
+            // 
+            // btnAverage
+            // 
+            this.btnAverage.Font = new System.Drawing.Font("EuroRoman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.btnAverage.Location = new System.Drawing.Point(83, 183);
+            this.btnAverage.Name = "btnAverage";
+            this.btnAverage.Size = new System.Drawing.Size(135, 30);
+            this.btnAverage.TabIndex = 4;
+            this.btnAverage.Text = "Calculate Average";
+            this.btnAverage.UseVisualStyleBackColor = true;
+            this.btnAverage.Click += new System.EventHandler(this.btnAverage_Click);
+            // 
+            // lblAverage
+            // 
+            this.lblAverage.AutoSize = true;
+            this.lblAverage.Font = new System.Drawing.Font("EuroRoman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.lblAverage.Location = new System.Drawing.Point(89, 225);
+            this.lblAverage.Name = "lblAverage";
+            this.lblAverage.Size = new System.Drawing.Size(129, 17);
+            this.lblAverage.TabIndex = 5;
+            this.lblAverage.Text = "The average is 100";
+            // 
+            // frmList
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.lblAverage);
+            this.Controls.Add(this.btnAverage);
+            this.Controls.Add(this.lstMarks);
+            this.Controls.Add(this.lblEnter);
+            this.Controls.Add(this.btnEnter);
+            this.Controls.Add(this.txtMark);
+            this.Name = "frmList";
+            this.Text = "List Average by Allison C";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtMark;
+        private System.Windows.Forms.Button btnEnter;
+        private System.Windows.Forms.Label lblEnter;
+        private System.Windows.Forms.ListBox lstMarks;
+        private System.Windows.Forms.Button btnAverage;
+        private System.Windows.Forms.Label lblAverage;
     }
 }
 
--- ListsAllisonC/ListForm.cs
+++ ListsAllisonC/ListForm.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿/*
+ * Created by: Allison Cook
+ * Created on: 14 May 2018
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #39 - Lists
+ * This program allows the users to enter marks into a list then calculates the average of the list
+*/
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -10,11 +17,43 @@ using System.Windows.Forms;
 
 namespace ListsAllisonC
 {
-    public partial class Form1 : Form
+    public partial class frmList : Form
     {
-        public Form1()
+        public frmList()
         {
             InitializeComponent();
+        }
+
+        private void btnEnter_Click(object sender, EventArgs e)
+        {
+            //declare local variables
+            int userMark = -1;
+
+            //if the user entered a number that is an int
+            try
+            {
+                //try to set the number to the variable
+                userMark = int.Parse(txtMark.Text);
+            }
+            catch
+            {
+                //display the message box
+                MessageBox.Show("Please enter a number between 0 and 100", "List Average");
+            }
+
+            //check if the number entered is between 0 and 100
+            if (userMark >= 0 && userMark<= 100)
+            {
+                //add the mark to the listbox
+                this.lstMarks.Items.Add(userMark);
+
+                //add it to the list
+            }
+        }
+
+        private void btnAverage_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
